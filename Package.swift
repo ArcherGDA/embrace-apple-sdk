7783065ed9e6fec141fd2bfe514c6541c4345bef
Joel Bell <56036846+Joel-Bell-okcupid@users.noreply.github.com>
2024-10-07 11:33:43 -0400

    Remove SwiftLintPlugin, Update headers for tracing (#1)
    * remove swiftlint plugin, update headers
    
    * clean up

--- Package.swift
+++ Package.swift
@@ -8,10 +8,16 @@ var targetPlugins: [Target.PluginUsage] = [.plugin(name: "SwiftLintPlugin", pack
 // Work around for plugin dependency being included in iOS target when using `xcodebuild test`
 // (See bin/xctest)
 // https://forums.swift.org/t/xcode-attempts-to-build-plugins-for-ios-is-there-a-workaround/57029
-if ProcessInfo.processInfo.environment["IS_XCTEST"] != nil ||
-    ProcessInfo.processInfo.environment["IS_ARCHIVE"] != nil {
-    targetPlugins.removeAll()
-}
+// if ProcessInfo.processInfo.environment["IS_XCTEST"] != nil ||
+//     ProcessInfo.processInfo.environment["IS_ARCHIVE"] != nil {
+//     targetPlugins.removeAll()
+// }
+
+// ARCHER work around
+//
+// Removes SwiftLintPlugin when resolving package dependencies to
+// bypass Xcode GUI prompt to enable or disable plugin
+targetPlugins.removeAll()
 
 let package = Package(
     name: "EmbraceIO",
